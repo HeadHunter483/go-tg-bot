77a6b26664b82bfe8c641ef67e1358d4b73345d4
Oleg Don <dragn613@gmail.com>
2022-05-08 02:14:58 +0300

    update bot.sh script

--- bot.sh
+++ bot.sh
@@ -2,25 +2,26 @@
 
 COMMAND=$1
 
-BOT_BIN=bot.exe
+BOT_BIN=bin/bot
+GOOSE_BIN=bin/goose
 
 case "$COMMAND" in 
 "run")
 # run go project
-go run bot.go db.go tg.go forismatic.go
+go run cmd/server/main.go
 ;;
 "build")
 # build go project only
-go build -o $BOT_BIN bot.go db.go tg.go forismatic.go
+go build -o $BOT_BIN server/main.go
 ;;
 "build-run")
 # build go project and run binary
 ./bot.sh build
 ./$BOT_BIN
 ;;
-"-ct")
-# create tables in db
-./bot.sh build
-./$BOT_BIN -ct
+"migrate")
+# build goose binary and run migrations
+go build -o $GOOSE_BIN goose/main.go
+./$GOOSE_BIN up
 ;;
 esac
